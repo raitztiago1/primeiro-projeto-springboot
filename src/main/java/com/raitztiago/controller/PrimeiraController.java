e011c83ed3edb3d415b8503398bdc04179e226cc
Tiago Raitz <60758361+raitztiago1@users.noreply.github.com>
2024-02-21 19:45:53 -0300

    QueryParams

--- src/main/java/com/raitztiago/controller/PrimeiraController.java
+++ src/main/java/com/raitztiago/controller/PrimeiraController.java
@@ -1,16 +1,30 @@
 package com.raitztiago.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Parabéns! Você criou a primeira rota!";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O Parametro é " + id;
     }
-    
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O Parametro com metodoComQueryParams é " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "O Parametro com metodoComQueryParams é " + allParams;
+    }
+
 }
